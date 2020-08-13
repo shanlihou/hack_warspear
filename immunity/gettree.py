d6a36402bfaeadd20f685a3134266683aea27bda
shanlihou <shanlihou@gmail.com>
2020-08-13 22:39:33 +0800

    up

--- immunity/gettree.py
+++ immunity/gettree.py
@@ -4,16 +4,12 @@ import immlib
 
 
 class Item(object):
-    def __init__(self, hp, ptr, entity_type):
+    def __init__(self, hp, ptr):
         self.hp = hp
         self.ptr = ptr
-        self.entity_type = entity_type
 
     def read_long(self, imm, offset):
         return imm.readLong(self.ptr + offset)
-
-    def get_target(self, imm):
-        return imm.readLong(self.ptr + 0x268)
 
     def print_iter(self, imm):
         yield '{:>10d}'.format(self.hp)
@@ -38,17 +34,9 @@ class ItemsInfo(object):
         self.items = {}
         self.hp_set = set()
 
-    def get_avatar(self):
-        for item_list in self.items.values():
-            for item in item_list:
-                if item.entity_type == 3:
-                    return item
-        
-        return None
-
-    def add_obj(self, hp, ptr, entity_type):
+    def add_obj(self, hp, ptr):
         self.items.setdefault(hp, [])
-        self.items[hp].append(Item(hp, ptr, entity_type))
+        self.items[hp].append(Item(hp, ptr))
         self.hp_set.add(hp)
 
     def parse_one_diff(self, imm, hp):
@@ -58,7 +46,7 @@ class ItemsInfo(object):
             map(lambda x: x.print_iter(imm), item_list))
 
         for i in range(400):
-            vals = list(map(next, iter_list))
+            vals = map(next, iter_list)
             vals_str = ''.join(vals)
 
             val_dic = {}
@@ -68,7 +56,7 @@ class ItemsInfo(object):
             if len(val_dic) == 2 and 1 in val_dic.values():
                 imm.log(vals_str)
 
-    def cmp_mon(self, imm):
+    def cmp(self, imm):
         iter_list = []
         iter_list.append(Item.print_tag())
         index = 1
@@ -83,28 +71,13 @@ class ItemsInfo(object):
 
             index += 1
 
-        def log_gen():
-            while 1:
-                vals = list(map(next, iter_list))
-                vals_str = ''.join(vals)
-                ret = (vals[index_203] == vals[index_203 + 1] and vals[index_203] != '{:>10d}'.format(0))
-                yield ret, vals_str
-        
-        lg = log_gen()
-        ret, str_print = next(lg)
-        imm.log(str_print)
-        for i in range(100):
-            rets = []
-            strs = []
-            for i in range(4):
-                ret, str_print = next(lg)
-                rets.append(ret)
-                strs.append(str_print)
-
-            imm.log(str(rets))
-            if any(rets):
-                for str_print in strs:
-                    imm.log(str_print)
+        for i in range(400):
+            vals = map(next, iter_list)
+            vals_str = ''.join(vals)
+            # if vals[index_203] == vals[index_203 + 1] and vals[index_203] !=
+            # '{:>10d}'.format(0):
+            if 1:
+                imm.log(vals_str)
 
 
 def read_tree(imm, node, func=None):
@@ -116,13 +89,11 @@ def read_tree(imm, node, func=None):
     value = imm.readLong(node + 16)
     ptr = imm.readLong(node + 20)
     hp = imm.readLong(ptr + 244)
-    entity_type = imm.readLong(ptr + 0x4)
-    
     read_tree(imm, left, func)
     read_tree(imm, right, func)
-    imm.log('{:#x},{}, {}'.format(ptr, hp, entity_type))
+    imm.log('{:#x},{}'.format(ptr, hp))
     if func:
-        func(hp, ptr, entity_type)
+        func(hp, ptr)
 
 
 def read_mons_info():
@@ -136,60 +107,21 @@ def read_mons_info():
     ii = ItemsInfo()
 
     read_tree(imm, root, ii.add_obj)
-    avatar = ii.get_avatar()
-    imm.log('avatar target:{:x}'.format(avatar.get_target(imm)))
+
     ii.parse_one_diff(imm, 203)
+
+
+def add_hook():
+    imm = immlib.Debugger()
+    hook_list = imm.listHooks()
+    imm.log(str(hook_list))
 
 
 def attach():
     imm = immlib.Debugger()
     imm.Attach(20624)
 
-def get_dead_body():
-    imm = immlib.Debugger()
-    # MOV EAX,DWORD PTR DS:[96EA30]
-    # MOV ECX,DWORD PTR DS:[EAX+10]
-    # MOV ECX,DWORD PTR DS:[ECX+34]
-    a = imm.readLong(0x96ea30)
-    a = imm.readLong(a + 0x10)
-    a = imm.readLong(a + 0x34)
-    a = imm.readLong(a + 0x38c)
-    a = imm.readLong(a + 0xe14)
-    a = imm.readLong(a + 0x78)
-    a = imm.readLong(a)
-    
-    a = imm.readLong(a + 0x8)
-    imm.log('ecx is:{:x}'.format(a))
-
-def get_switch_choice():
-    imm = immlib.Debugger()
-    #ecx = imm.getRegs()['ESI']
-    #imm.log('ecx:{:x}'.format(ecx))
-    EAX = imm.readLong(0x96EA30)
-    imm.log('EAX:{:x}'.format(EAX))
-    ECX = imm.readLong(EAX + 0x10)
-    imm.log('ECX:{:x}'.format(ECX))
-    ECX = imm.readLong(ECX + 0x34)
-    imm.log('ECX:{:x}'.format(ECX))
-    # 0073FAFD  |. 8B86 8C030000  MOV EAX,DWORD PTR DS:[ESI+38C]
-    # 0073FB03  |. 8B80 140E0000  MOV EAX,DWORD PTR DS:[EAX+E14]
-    # 0073FB09  |. 8B40 78        MOV EAX,DWORD PTR DS:[EAX+78]
-    # 0073FB0C  |. 8B08           MOV ECX,DWORD PTR DS:[EAX]
-    EAX = imm.readLong(ECX + 0x38c)
-    imm.log('EAX:{:x}'.format(EAX))
-    EAX = imm.readLong(EAX + 0xe14)
-    imm.log('EAX:{:x}'.format(EAX))
-    EAX = imm.readLong(EAX + 0x78)
-    imm.log('EAX:{:x}'.format(EAX))
-    ECX = imm.readLong(EAX + 0x0)
-    imm.log('ECX:{:x}'.format(ECX))
-
-    # 0073FB26  |> 8B51 0C        MOV EDX,DWORD PTR DS:[ECX+C]
-    EDX = imm.readLong(ECX + 0xc)
-    imm.log('EDX:{:x}'.format(EDX))
-    a = imm.readLong(EDX + 0xc)
-    imm.log('choice:{:x}'.format(a))
 
 def main(args):
-    get_switch_choice()
+    add_hook()
     return "[*] PyCommand Executed!"
---
+++ immunity/hook_war.py
@@ -0,0 +1,118 @@
+from immlib import *
+import re
+
+ADD_HOOK = 1
+UN_HOOK = 2
+HOOK_WITH_ARG = 3
+HOOK_TEST = 4
+
+#############################################################################
+class HookWar(LogBpHook):
+    def __init__(self, desp, print_str):
+        LogBpHook.__init__(self)
+        self.description = desp
+        self.print_str = print_str
+
+    #########################################################################
+    def run(self,regs):
+        '''
+
+        '''
+        imm = Debugger()
+        high_p = self.print_str.upper()
+        if high_p in regs:
+            imm.log('{}:{}'.format(high_p, regs[high_p]))
+        else:
+            imm.log("{}:{}".format(self.print_str, str(regs)))
+
+
+class InstallHook(object):
+    def __init__(self, imm):
+        self.imm = imm
+        self.addr_list = []
+        self.addr_pre = [
+            # '0084E1B0',
+            # '0083A600',
+            # '0071D2A0',
+            # '005A9890',
+            # '007BC2A0',
+            # '00781C60',
+            '005A9210',
+            '0073FA90',
+        ]
+
+    def add_hook(self, addr):
+        self.imm.log(addr)
+        self.addr_list.append(addr)
+        print_str = '->'.join(self.addr_list)
+        int_addr = int(addr, 16)
+
+        hooker = HookWar(addr, print_str)
+        ret = hooker.add(hooker.description,  int_addr)
+        if ret == -1:
+            self.imm.log("Hooking add failed:{}!".format(addr))
+
+    def add_pre_set(self):
+        for index, addr in enumerate(self.addr_pre):
+            int_addr = int(addr, 16)
+            self.imm.log(addr)
+            print_str = '->'.join(self.addr_pre[:index + 1])
+
+            hooker = HookWar(addr, print_str)
+            ret = hooker.add(hooker.description, int_addr)
+            if ret == -1:
+                self.imm.log("Hooking add failed:{}!".format(addr))
+
+    def get_hook_info(self):
+        pat = re.compile(r'warspear\.(\w+)\s+warspear\.(\w+)')
+        with open(r'E:\shgithub\cpp\hackWarspear\hackWarspear\info\dead.txt') as fr:
+            for line in fr:
+                find = pat.search(line)
+                if find:
+                    self.add_hook(find.groups()[0])
+
+
+def main(args):
+    imm = Debugger()
+    imm.log(str(args))
+    if len(args) < 1:
+        return 'hook war failed'
+
+    arg = int(args[0])
+    if arg == ADD_HOOK:
+        ih = InstallHook(imm)
+        ih.add_pre_set()
+
+        # hooker = HookWar()
+        # hooker.description = 'hello'
+        # ret = hooker.add(hooker.description,  0x4B2576)
+        # if ret == -1:
+        #     imm.log("Hooking add failed!")
+        # else:
+        #     imm.addKnowledge("ckz_hook", hooker.description)
+    elif arg == UN_HOOK:
+        desp = imm.getKnowledge("ckz_hook")
+        if desp:
+            imm.log(desp)
+
+            imm.removeHook(desp)
+                #now forget about that hook
+            imm.forgetKnowledge("ckz_hook")
+    elif arg == HOOK_WITH_ARG:
+        if len(args) != 3:
+            return 'hook war args wrong'
+
+        addr = args[1]
+        print_str = args[2]
+        h = HookWar(addr, print_str)
+        ret = h.add(h.description,  int(addr, 16))
+        if ret == -1:
+            imm.log("hook with arg failed!")
+    elif arg == HOOK_TEST:
+        if len(args) != 3:
+            return 'hook war test args wrong'
+
+        print_str = args[2]
+        imm.log(str(imm.decodeAddress(print_str)))
+
+    return 'hello'
